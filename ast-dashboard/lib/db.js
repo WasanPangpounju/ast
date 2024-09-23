b4040a903bb30b291ec59ce8f1909995bcc791be
WasanPangpounju <friendlydev.net@gmail.com>
2024-09-23 17:08:11 +0700

    update

--- ast-dashboard/lib/db.js
+++ ast-dashboard/lib/db.js
@@ -1,25 +1,26 @@
 // lib/db.js
 import mariadb from 'mariadb';
 
+// Create a connection pool
 const pool = mariadb.createPool({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT || 3306,
-  connectionLimit: 10,  // Increase this value
-  acquireTimeout: 30000 // Increase timeout (optional)
+  port: process.env.DATABASE_PORT,
+  connectionLimit: 5,
 });
 
-export async function query(sql, params) {
+// Function to query the database
+export async function query(sql, values) {
   let conn;
   try {
     conn = await pool.getConnection();
-    const rows = await conn.query(sql, params);
-    return rows;
-  } catch (err) {
-    throw err;
+    const result = await conn.query(sql, values);
+    return result;
+  } catch (error) {
+    throw error;
   } finally {
-    if (conn) conn.end();  // Always release the connection back to the pool
+    if (conn) conn.release(); // Ensure the connection is released
   }
 }
