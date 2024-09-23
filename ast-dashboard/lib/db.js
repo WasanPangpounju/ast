2cee720768deb39579ebb6ef1f0062f74450d800
WasanPangpounju <friendlydev.net@gmail.com>
2024-09-23 17:47:43 +0700

    update

--- ast-dashboard/lib/db.js
+++ ast-dashboard/lib/db.js
@@ -2,12 +2,20 @@
 import mariadb from 'mariadb';
 
 // Create a connection pool
+// const pool = mariadb.createPool({
+//   host: process.env.DATABASE_HOST,
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DATABASE_NAME,
+//   port: process.env.DATABASE_PORT,
+//   connectionLimit: 5,
+// });
 const pool = mariadb.createPool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT,
+  host: localhost,
+  user: fddashboard,
+  password: fd9096390,
+  database: ast,
+  port: 3306,
   connectionLimit: 5,
 });
 
