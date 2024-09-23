ac78205cc4c79e3b549893c0a4eefc0af7dcb85e
WasanPangpounju <friendlydev.net@gmail.com>
2024-09-23 18:05:25 +0700

    update

--- ast-dashboard/lib/db.js
+++ ast-dashboard/lib/db.js
@@ -1,6 +1,7 @@
 // src/lib/db.js
 import mysql from 'mysql2/promise';
 
+
 // Create a connection pool
 const pool = mysql.createPool({
   host: process.env.DATABASE_HOST,
@@ -8,7 +9,8 @@ const pool = mysql.createPool({
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   port: process.env.DATABASE_PORT || 3306,
-  connectionLimit: 10,  // Limit simultaneous connections
+  connectionLimit: 10,  // Limit of concurrent connections
+  acquireTimeout: 30000 // 30 seconds timeout (default is 10 seconds)  
 });
 
 // A helper function to query the database
