da0686fa24ce41ee80b329c789dccd1e8c8876d4
WasanPangpounju <friendlydev.net@gmail.com>
2024-09-23 16:53:01 +0700

    update

--- ast-dashboard/src/app/api/ast/route.js
+++ ast-dashboard/src/app/api/ast/route.js
@@ -3,7 +3,7 @@ import { query } from '../../../../lib/db';
 
 export async function GET(request) {
   try {
-    const results = await query('SELECT * FROM users');
+    const results = await query('SELECT * FROM materialstores');
     return new Response(JSON.stringify(results), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify({ message: 'Database error', error }), { status: 500 });
