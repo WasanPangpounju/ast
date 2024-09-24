90123123026bed32fa187ffb59b21b4c8cd94d63
WasanPangpounju <friendlydev.net@gmail.com>
2024-09-24 15:17:05 +0700

    update

--- ast-dashboard/src/app/dashboard/components/Sidebar.js
+++ ast-dashboard/src/app/dashboard/components/Sidebar.js
@@ -7,9 +7,13 @@ export default function Sidebar() {
         <div className="sidebar">
             <h2>Dashboard</h2>
             <ul>
-                <li><Link href="/dashboard/home">Home</Link></li>
-                <li><Link href="/dashboard/users">Users</Link></li>
-                <li><Link href="/dashboard/settings">Settings</Link></li>
+                <li><Link href="/dashboard/home">หน้าแรก</Link></li>
+                <li><Link href="/dashboard/users">ผู้ใช้งาน</Link></li>
+                <li><Link href="/dashboard/order">ออเดอร์</Link></li>
+                <li><Link href="/dashboard/stock">สต๊อกผ้า</Link></li>
+                <li><Link href="/dashboard/stockcustomer">สต๊อกผ้าฝากจัดเก็บ</Link></li>
+                <li><Link href="/dashboard/stockmaterial">สต๊อกวัตถุดิบ</Link></li>
+                <li><Link href="/dashboard/settings">ตั้งค่า</Link></li>
             </ul>
         </div>
     );
