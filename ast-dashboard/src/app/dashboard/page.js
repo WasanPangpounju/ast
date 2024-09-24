9590069fc6c881cc8576fc7262e64cdb15e3ab6b
WasanPangpounju <friendlydev.net@gmail.com>
2024-09-24 15:47:48 +0700

    update

--- ast-dashboard/src/app/dashboard/page.js
+++ ast-dashboard/src/app/dashboard/page.js
@@ -2,7 +2,9 @@
 
 'use client'; // This is required for Client-side behavior
 
-import Sidebar from './components/Sidebar';
+// import Sidebar from './components/Sidebar';
+// import DashboardContent from './components/DashboardContent';
+import Sidebar from '../../components/Sidebar'; // Corrected path
 import DashboardContent from './components/DashboardContent';
 
 export default function DashboardPage() {
