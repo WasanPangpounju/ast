56a77e00077af887b72493917a498ecd93a707ea
dechawat <69790549+dechawat0233@users.noreply.github.com>
2024-10-01 15:36:42 +0700

    edit

--- ast-dashboard/src/app/dashboard/users/page.js
+++ ast-dashboard/src/app/dashboard/users/page.js
@@ -32,7 +32,7 @@ export default function UsersPage() {
 
     return (
         <div className="dashboard-container">
-            <Sidebar /> 
+            {/* <Sidebar />  */}
             {/* Sidebar is included here */}
             <div className="dashboard-content">
                 <h1>Users Management</h1>
