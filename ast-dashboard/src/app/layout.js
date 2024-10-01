411fd2761c16bb66336430f0d64446a87b599572
dechawat <69790549+dechawat0233@users.noreply.github.com>
2024-10-01 16:57:28 +0700

    edit

--- ast-dashboard/src/app/layout.js
+++ ast-dashboard/src/app/layout.js
@@ -1,9 +1,11 @@
+'use client';
+
 import localFont from "next/font/local";
 import "./globals.css";
 import Sidebar from "./dashboard/components/Sidebar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { useEffect } from 'react';
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect } from "react";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -21,9 +23,9 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  // useEffect(() => {
-  //   import('bootstrap/dist/js/bootstrap.bundle.min.js'); // Ensure Bootstrap JS is loaded after DOM is ready
-  // }, []);
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.min.js'); // Ensure Bootstrap JS is loaded after DOM is ready
+  }, []);
   return (
     <html lang="en">
       <body
