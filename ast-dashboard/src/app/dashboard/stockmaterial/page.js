50dec0884a31e43f3c9a01ab498a1e80af64297a
dechawat <69790549+dechawat0233@users.noreply.github.com>
2024-10-01 16:21:38 +0700

    edit

--- ast-dashboard/src/app/dashboard/stockmaterial/page.js
+++ ast-dashboard/src/app/dashboard/stockmaterial/page.js
@@ -2,9 +2,91 @@
 
 export default function Users() {
   return (
-      <div>
-          <h1>User Management</h1>
-          <p>Manage all users from this section.</p>
+    <div>
+      {/* <h1>User Management</h1>
+          <p>Manage all users from this section.</p> */}
+
+      <div class="content-wrapper">
+        <div class="">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="index.php">หน้าหลัก</a>
+            </li>
+            <li class="breadcrumb-item">
+              <a href="#">วัตถุดิบ</a>
+            </li>
+            <li class="breadcrumb-item active">สต๊อกวัตถุดิบคงเหลือ</li>
+          </ol>
+        </div>
+
+        <div class="content-header">
+          <div class="container-fluid">
+            <div class="row mb-2 Header">
+              <div class="col">
+                <h1 class="m-0">
+                  <i class="nav-icon fas fa fa-arrow-circle-right"></i>{" "}
+                  สต๊อกวัตถุดิบ
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="content">
+          <div class="box-from">
+            <div class="">
+              <form method="post">
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <label for="yarntype">ชนิดด้าย</label>
+                      <select name="yarnType" class="form-control">
+                        <option disabled selected value="">
+                          เลือกชนิดได้
+                        </option>
+
+                        <option value="{{ $key }}">1</option>
+                        <option value="{{ $key }}">2</option>
+                        <option value="{{ $key }}">3</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <label for="supplier">บริษัท</label>
+
+                      <select name="supplier" class="form-control">
+                        <option disabled selected value="">
+                          เลือกบริษัท
+                        </option>
+
+                        <option value="{{ $key }}">1</option>
+                        <option value="{{ $key }}">2</option>
+                        <option value="{{ $key }}">3</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <button
+                        type="submit"
+                        name="submit"
+                        value="search"
+                        id="search"
+                        class="btn btn-etc"
+                        style="margin-top:2em;"
+                      >
+                        {" "}
+                        <i class="fas fa-folder"></i> ค้นหา
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
   );
 }
