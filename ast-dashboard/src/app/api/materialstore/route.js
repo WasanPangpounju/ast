222bcc499925148c9d868cdaf3865463571f1fec
dechawat <69790549+dechawat0233@users.noreply.github.com>
2024-10-02 17:26:16 +0700

    edit

--- ast-dashboard/src/app/api/materialstore/route.js
+++ ast-dashboard/src/app/api/materialstore/route.js
@@ -2,10 +2,10 @@ import { query } from '../../../../lib/db';
 
 export async function GET() {
   try {
-    const materialstore = await query('SELECT * FROM materialstore');
-    return new Response(JSON.stringify(materialstore), { status: 200 });
+    const materialstores = await query('SELECT * FROM materialstores');
+    return new Response(JSON.stringify(materialstores), { status: 200 });
   } catch (error) {
-    console.error('Error fetching materialstore:', error);
+    console.error('Error fetching materialstores:', error);
     return new Response(JSON.stringify({ message: 'Database error', error }), { status: 500 });
   }
 }
