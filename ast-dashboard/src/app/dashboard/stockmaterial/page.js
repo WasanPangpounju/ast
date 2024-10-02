18d59236bd3c922f755186c82685005a9a77015e
dechawat <69790549+dechawat0233@users.noreply.github.com>
2024-10-02 11:00:03 +0700

    edit

--- ast-dashboard/src/app/dashboard/stockmaterial/page.js
+++ ast-dashboard/src/app/dashboard/stockmaterial/page.js
@@ -35,8 +35,59 @@ export default function Users() {
         <div class="content">
           <div class="box-from">
             <div class="">
-            
+              <form method="post">
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <label for="yarntype">ชนิดด้าย</label>
+                      <select name="yarnType" class="form-control">
+                        <option disabled selected value="">
+                          เลือกชนิดได้
+                        </option>
+
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <label for="supplier">บริษัท</label>
+
+                      <select name="supplier" class="form-control">
+                        <option disabled selected value="">
+                          เลือกบริษัท
+                        </option>
+
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <button
+                        type="submit"
+                        name="submit"
+                        value="search"
+                        id="search"
+                        class="btn btn-etc"
+                        style="margin-top:2em;"
+                      >
+                        <i class="fas fa-folder"></i> ค้นหา
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
             </div>
+          </div>
+        </div>
+        <div class="content">
+          <div class="box-from">
+            <div class=""></div>
           </div>
         </div>
       </div>
