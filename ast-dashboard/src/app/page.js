3dca89b8f418c4b6d185f083a43db6c73b15544f
dechawat <69790549+dechawat0233@users.noreply.github.com>
2024-10-04 15:15:40 +0700

    edit

--- ast-dashboard/src/app/page.js
+++ ast-dashboard/src/app/page.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.min.js'); // Import Bootstrap JS
+  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {/* <Sidebar/> */}
