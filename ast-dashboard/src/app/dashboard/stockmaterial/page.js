1e79545e03fb3b285727362b73cfdd4df157807b
dechawat <69790549+dechawat0233@users.noreply.github.com>
2024-10-04 15:59:57 +0700

    edit

--- ast-dashboard/src/app/dashboard/stockmaterial/page.js
+++ ast-dashboard/src/app/dashboard/stockmaterial/page.js
@@ -311,7 +311,10 @@ export default function Users() {
         </div>
         <section class="Frame">
           <div class="row">
-            <div class="col-md-12">
+          <div class="col-md-5">
+              
+            </div>
+            <div class="col-md-2">
               <div class="row">
                 <div class="col-md-12">
                   ด้าย<a> {spoolSum} </a>ลูก
@@ -326,6 +329,9 @@ export default function Users() {
                 </div>
               </div>
             </div>
+            <div class="col-md-5">
+              
+              </div>
           </div>
         </section>
       </div>
