d7d1094834aebbf4fb2a6f31cd6e4e27b77281cd
dechawat <69790549+dechawat0233@users.noreply.github.com>
2024-10-28 10:34:10 +0700

    edit

--- ast-dashboard/src/app/api/astPurchaseorder/route.js
+++ ast-dashboard/src/app/api/astPurchaseorder/route.js
@@ -2,7 +2,7 @@ import { query } from '../../../../lib/db';
 
 export async function GET() {
   try {
-    const astPurchaseorder = await query('SELECT * FROM astPurchaseorder');
+    const astPurchaseorder = await query('SELECT * FROM ast_purchaseorders');
     return new Response(JSON.stringify(astPurchaseorder), { status: 200 });
   } catch (error) {
     console.error('Error fetching astPurchaseorder:', error);
