7f65b4237ffc229af02fbda929cbf71d8bf5d8c4
dechawat <69790549+dechawat0233@users.noreply.github.com>
2024-11-01 10:40:08 +0700

    edit

--- ast-dashboard/src/app/dashboard/components/Sidebar.js
+++ ast-dashboard/src/app/dashboard/components/Sidebar.js
@@ -5,19 +5,37 @@ import Link from "next/link";
 export default function Sidebar() {
   return (
     <div className="sidebar" style={{ backgroundColor: "#091e91" }}>
-                      <img src="assets/images/logo.png" alt="Logo" class="brand-image"/>
+      <img
+        src="assets/images/logo.png"
+        alt="Logo"
+        class="brand-image"
+        style={{ width: "50%", height: "50%" }}
+      />
 
-                <span class="brand-text font-weight-light">ASIA TEXTILE CO., LTD.</span>
-                <ul>
-                <li><Link href="/dashboard/home">หน้าแรก</Link></li>
-                <li><Link href="/dashboard/users">ผู้ใช้งาน</Link></li>
-                <li><Link href="/dashboard/order">ออเดอร์</Link></li>
-                <li><Link href="/dashboard/stock">สต๊อกผ้า</Link></li>
-                <li><Link href="/dashboard/stockcustomer">สต๊อกผ้าฝากจัดเก็บ</Link></li>
-                <li><Link href="/dashboard/stockmaterial">สต๊อกวัตถุดิบ</Link></li>
-                <li><Link href="/dashboard/settings">ตั้งค่า</Link></li>
-            </ul>
-     
+      <span class="brand-text font-weight-light">ASIA TEXTILE CO., LTD.</span>
+      <ul>
+        <li>
+          <Link href="/dashboard/home">หน้าแรก</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users">ผู้ใช้งาน</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/order">ออเดอร์</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/stock">สต๊อกผ้า</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/stockcustomer">สต๊อกผ้าฝากจัดเก็บ</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/stockmaterial">สต๊อกวัตถุดิบ</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/settings">ตั้งค่า</Link>
+        </li>
+      </ul>
     </div>
   );
 }
